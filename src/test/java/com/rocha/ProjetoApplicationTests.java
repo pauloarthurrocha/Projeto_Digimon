bc2543be89bbaeb1f0965ac3f77991098b57415f
Paulo Arthur Rocha <paulo.arthu@hotmail.com>
2020-11-30 10:15:15 -0300

    first commit

---
+++ src/test/java/com/rocha/ProjetoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rocha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
